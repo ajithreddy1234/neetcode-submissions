5fb37fb0e1fe6fc47227f134b3b32f4026e8ea0e
ajithreddy1234 <ajithreddy.pr@gmail.com>
2026-08-17 16:25:45 +0530

    Time: 67 ms (13.3%), Space: 21 MB (30.27%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -1,14 +1,10 @@
-class Pair():
-    def __init__(self,val,x):
-        self.val=val
-        self.x=x
-    def __lt__(self,other):
-        return abs(self.val-self.x)>abs(other.val-self.x) or (abs(self.val-self.x)==abs(other.val-self.x) and self.val>other.val)
+
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        heap=[]
-        for val in arr:
-            heapq.heappush(heap,Pair(val,x))
-            if len(heap)>k:
-                heapq.heappop(heap)
-        return sorted([m.val for m in heap])
+        def cmp(a,b):
+            if abs(a-x)<abs(b-x) or (abs(a-x)==abs(b-x) and a<b):
+                return -1
+            else:
+                return 1
+        arr.sort(key=cmp_to_key(cmp))
+        return sorted(arr[:k])
