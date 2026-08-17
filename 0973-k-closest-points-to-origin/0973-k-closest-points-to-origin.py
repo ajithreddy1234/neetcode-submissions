7d1bcc9784327370dad97b102626a62be50ee76c
ajithreddy1234 <ajithreddy.pr@gmail.com>
2026-08-17 15:54:35 +0530

    Time: 37 ms (94.83%), Space: 23.8 MB (95.07%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,18 +1,6 @@
-import math
-class Pair:
-    def __init__(self,coord):
-        self.coord=coord
-    def __lt__(self,other):
-        return (self.coord[0]**2+self.coord[1]**2)>(other.coord[0]**2+other.coord[1]**2)
-
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        heap=[]
-        for point in points:
-            heapq.heappush(heap,Pair(point))
-            if len(heap)>k:
-                heapq.heappop(heap)
-        print(heap)
-        return [p.coord for p in heap]
+        points.sort(key=lambda x: x[0]**2+x[1]**2)
+        return points[:k]
 
         
