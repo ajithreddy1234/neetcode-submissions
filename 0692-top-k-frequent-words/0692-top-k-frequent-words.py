238860d15b2403488036023bc473a6d04eed34c8
ajithreddy1234 <ajithreddy.pr@gmail.com>
2026-08-17 15:41:26 +0530

    Time: 3 ms (54.35%), Space: 19.5 MB (19.22%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -1,7 +1,38 @@
+from collections import Counter
+import heapq
+from typing import List
+
+
+class Pair:
+    def __init__(self, freq, word):
+        self.freq = freq
+        self.word = word
+
+    def __lt__(self, other):
+        # Lower frequency = worse
+        if self.freq != other.freq:
+            return self.freq < other.freq
+
+        # Same frequency:
+        # lexicographically larger word = worse
+        return self.word > other.word
+
+
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        x=Counter(words)
-        arr=list(x.items())
-        arr.sort(key=lambda x:(-x[1],x[0]))
-        return [word for word,freq in arr[:k]]
-        
+        count = Counter(words)
+
+        heap = []
+
+        for word, freq in count.items():
+            heapq.heappush(heap, Pair(freq, word))
+
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        ans = []
+
+        while heap:
+            ans.append(heapq.heappop(heap).word)
+
+        return ans[::-1]
