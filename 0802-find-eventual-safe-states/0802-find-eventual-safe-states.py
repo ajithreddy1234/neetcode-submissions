79cd07352905386ded5c52420388aacbfabe5818
ajithreddy1234 <ajithreddy.pr@gmail.com>
2026-08-26 16:13:22 +0530

    Time: 46 ms (57.03%), Space: 25.9 MB (72.26%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -1,27 +1,27 @@
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
         n=len(graph)
-        hot=[0]*n
-        def dfs(node):
-            if hot[node]==1:
-                return False
-            if hot[node]==2:
-                return True
-            if hot[node]==3:
-                return False
-            hot[node]=1
+        reverse=[[] for _ in range(n)]
+        outdegree=[0]*n
+        for node in range(n):
+            outdegree[node]=len(graph[node])
             for nei in graph[node]:
-                if not dfs(nei):
-                    hot[nei]=3
-                    hot[node]=3
-                    return False
-            hot[node]=2
-            return True
-        final=[]
-        for i in range(n):
-            if dfs(i):
-                final.append(i)
-        return final
+                reverse[nei].append(node)
+        dq=deque()
+        for i in range(len(outdegree)):
+            if outdegree[i]==0:
+                dq.append(i)
+        topo=[]
+        while dq:
+            x=dq.popleft()
+            topo.append(x)
+            for nei in reverse[x]:
+                outdegree[nei]-=1
+                if outdegree[nei]==0:
+                    dq.append(nei)
+        return sorted(topo)
+
+
 
 
                 
