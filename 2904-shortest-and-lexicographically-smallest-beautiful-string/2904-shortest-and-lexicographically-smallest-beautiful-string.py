f03c6efb1c4f1d53daa2117ff1d923a34d0495bf
ajithreddy1234 <ajithreddy.pr@gmail.com>
2026-08-26 12:40:55 +0530

    Time: 3 ms (53.15%), Space: 19.5 MB (6.76%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.py
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.py
@@ -1,27 +1,25 @@
 class Solution:
     def shortestBeautifulSubstring(self, s: str, k: int) -> str:
-        dic={"0":0,"1":0}
+        ans=""
+        ones=0
         l=0
-        res=0
-        opt_l=0
-        opt_r=len(s)-1
-        entered=False
         for r in range(len(s)):
-            dic[s[r]]+=1
-            while dic["1"]>=k:
-                if dic["1"]==k:
-                    entered=True
-                    if r-l+1<opt_r-opt_l+1:
-                        opt_l=l
-                        opt_r=r
-                    elif r-l+1==opt_r-opt_l+1:
-                        if s[opt_l:opt_r+1]>s[l:r+1]:
-                            opt_l=l
-                            opt_r=r
-                dic[s[l]]-=1
+            if s[r]=="1":
+                print(r)
+                ones+=1
+            while ones>k:
+                if s[l]=="1":
+                    ones-=1
                 l+=1
-        
-        return s[opt_l:opt_r+1] if entered else ""
+            while l<=r and ones==k and s[l]=="0":
+                l+=1
+            if ones==k:
+                cnadi=s[l:r+1]
+                if (ans=="" or len(ans)>len(cnadi) or(len(ans)==len(cnadi) and  ans>cnadi)):
+                    ans=cnadi
+        return ans
+            
+
 
 
         
