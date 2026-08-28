8f3b530c7608798a93f07ca527108f0f1b947dfc
ajithreddy1234 <ajithreddy.pr@gmail.com>
2026-08-28 13:23:17 +0530

    Time: 0 ms (100%), Space: 19.4 MB (7.53%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -0,0 +1,63 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        count=len(matrix)*len(matrix[0])
+        i=0
+        j=0
+        rows=len(matrix)
+        cols=len(matrix[0])
+        print(rows,cols)
+        visited=set()
+        final=[]
+        def check(i,j):
+                for dx,dy in [(1,0),(0,1),(-1,0),(0,-1)]:
+                    nx,ny=i+dx,j+dy
+                    if 0<=nx<rows and 0<=ny<cols and (nx,ny) not in visited:
+                        return nx,ny
+                return i,j
+        while True:
+            while j<cols and (i,j) not in visited:
+                final.append(matrix[i][j])
+                visited.add((i,j))
+                j+=1
+            if len(final)==count:
+                return final
+            j-=1
+            i,j=check(i,j)
+            if (i,j) in visited:
+                return final
+            while i<rows and (i,j) not in visited:
+                final.append(matrix[i][j])
+                visited.add((i,j))
+                i+=1
+            i-=1
+            if len(final)==count:
+                return final
+            i,j=check(i,j)
+            if (i,j) in visited:
+                return final
+            while j>=0 and (i,j) not in visited:
+                final.append(matrix[i][j])
+                visited.add((i,j))
+                j-=1
+            j+=1
+            if len(final)==count:
+                return final
+            i,j=check(i,j)
+            if (i,j) in visited:
+                return final
+            while i>=0 and (i,j) not in visited:
+                final.append(matrix[i][j])
+                visited.add((i,j))
+                i-=1
+            if len(final)==count:
+                return final
+            i+=1
+            i,j=check(i,j)
+            if (i,j) in visited:
+                return final
+
+
+
+
+            
+
