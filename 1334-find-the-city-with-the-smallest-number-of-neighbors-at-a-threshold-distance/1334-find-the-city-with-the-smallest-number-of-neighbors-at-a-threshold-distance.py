ef4d09a4da742de2be181041841a95893914d4a9
ajithreddy1234 <ajithreddy.pr@gmail.com>
2026-09-02 22:12:55 +0530

    Time: 270 ms (41.55%), Space: 20.3 MB (62.39%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
@@ -1,23 +1,46 @@
 class Solution:
-    def findTheCity(self, V: int, edges: List[List[int]], distanceThreshold: int) -> int:
-        fin = [set() for _ in range(V)]
-        dist=[[float("inf") for _ in range(V)] for _ in range(V)]
+    def findTheCity(
+        self,
+        V: int,
+        edges: List[List[int]],
+        distanceThreshold: int
+    ) -> int:
+
+        dist = [
+            [float("inf")] * V
+            for _ in range(V)
+        ]
+
         for i in range(V):
-            dist[i][i]=0
-        for u,v,w in edges:
-            dist[u][v]=min(dist[u][v],w)
-            dist[v][u]=min(dist[v][u],w)
+            dist[i][i] = 0
+
+        for u, v, w in edges:
+            dist[u][v] = min(dist[u][v], w)
+            dist[v][u] = min(dist[v][u], w)
+
+        # First finish Floyd-Warshall
         for k in range(V):
             for u in range(V):
                 for v in range(V):
-                    dist[u][v]=min(dist[u][k]+dist[k][v],dist[u][v])
-                    if dist[u][v]<=distanceThreshold:
-                        if u!=v:
-                            fin[u].add(v)
-                            fin[v].add(u)
-        heap=[]
-        for key,values in enumerate(fin):
-            heapq.heappush(heap,(len(values),-key))
-        return -heapq.heappop(heap)[1]
+                    dist[u][v] = min(
+                        dist[u][v],
+                        dist[u][k] + dist[k][v]
+                    )
 
-        
+        # NOW all distances are final
+        min_count = float("inf")
+        min_val = -1
+
+        for u in range(V):
+
+            count = 0
+
+            for v in range(V):
+                if u != v and dist[u][v] <= distanceThreshold:
+                    count += 1
+
+            if count <= min_count:
+                min_count = count
+                min_val = u
+
+        return min_val
