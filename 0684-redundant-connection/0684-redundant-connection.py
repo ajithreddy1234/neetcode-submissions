4218c91a16bd5f000672d542e47cc65bc49075fe
ajithreddy1234 <ajithreddy.pr@gmail.com>
2026-09-03 11:44:19 +0530

    Time: 6 ms (22.64%), Space: 19.7 MB (50.92%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -0,0 +1,33 @@
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        n=0
+        for m,l in edges:
+            n=max(n,m,l)
+        class DSU:
+            def __init__(self,n):
+                self.parent=[i for i in range(n+1)]
+                self.size=[1]*(n+1)
+            def find(self,x):
+                print(x)
+                if self.parent[x]!=x:
+                    self.parent[x]=self.find(self.parent[x])
+                return self.parent[x]
+            def union(self,x,y):
+                A=self.find(x)
+                B=self.find(y)
+                if A==B:
+                    return False
+                if self.size[A]<self.size[B]:
+                    A,B=B,A
+                self.parent[B]=A
+                self.size[A]+=self.size[B]
+                return True
+        D=DSU(n)
+        for u,v in edges:
+            if not D.union(u,v):
+                return [u,v]
+
+
+
+        
