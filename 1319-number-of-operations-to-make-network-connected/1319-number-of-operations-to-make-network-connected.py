03b31ed5db269740d3c8a19ffab37a9250ffad36
ajithreddy1234 <ajithreddy.pr@gmail.com>
2026-09-04 01:00:19 +0530

    Time: 31 ms (80.48%), Space: 32.8 MB (74.39%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
@@ -1,35 +1,41 @@
 class Solution:
     def makeConnected(self, n: int, connections: List[List[int]]) -> int:
-        class DSU:
-            def __init__(self,n):
-                self.parents=[i for i in range(n)]
-                self.size=[1]*n
-                self.parent=0
-            def find(self,x):
-                if self.parents[x]!=x:
-                    self.parents[x]=self.find(self.parents[x])
-                return self.parents[x]
-            def union(self,x,y):
-                A=self.find(x)
-                B=self.find(y)
-                if A==B:
-                    return False
-                if self.size[A]<self.size[B]:
-                    A,B=B,A
-                self.parents[B]=A
-                self.size[A]+=self.size[B]
-                if self.size[self.parent]<self.size[A]:
-                    self.parent=A
-                return True
-        bad=0
-        d=DSU(n)
-        for u,v in connections:
-            if not d.union(u,v):
-                bad+=1
-        mg=len(set(d.find(i) for i in range(n)))
-        if mg-1<=bad:
-            return mg-1
-        else:
+
+        # Not enough cables to ever connect n computers
+        if len(connections) < n - 1:
             return -1
 
-        
+        class DSU:
+            def __init__(self, n):
+                self.parents = [i for i in range(n)]
+                self.size = [1] * n
+
+            def find(self, x):
+                if self.parents[x] != x:
+                    self.parents[x] = self.find(self.parents[x])
+                return self.parents[x]
+
+            def union(self, x, y):
+                A = self.find(x)
+                B = self.find(y)
+
+                if A == B:
+                    return False
+
+                if self.size[A] < self.size[B]:
+                    A, B = B, A
+
+                self.parents[B] = A
+                self.size[A] += self.size[B]
+
+                return True
+
+        d = DSU(n)
+
+        components = n
+
+        for u, v in connections:
+            if d.union(u, v):
+                components -= 1
+
+        return components - 1
