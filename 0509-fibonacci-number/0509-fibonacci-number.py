af3c8aea2b88f8c879b0e62a301faa2ce2e43c9b
ajithreddy1234 <ajithreddy.pr@gmail.com>
2026-09-07 12:10:20 +0530

    Time: 43 ms (79.6%), Space: 19.3 MB (19.52%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,13 +1,11 @@
 class Solution:
     def fib(self, n: int) -> int:
-        memo={}
-        def dp(i):
+        dp=[-1]*(n+1)
+        for i in range(n+1):
             if i<=1:
-                return i
-            if i in memo:
-                return memo[i]
-            memo[i]=dp(i-1)+dp(i-2)
-            return memo[i]
-        return dp(n)
+                dp[i]=i
+            else:
+                dp[i]=dp[i-1]+dp[i-2]
+        return dp[n]
 
         
