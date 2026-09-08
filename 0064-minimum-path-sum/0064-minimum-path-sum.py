e2e21a5d4b53114a9c49edf1659da74819f1cdc4
ajithreddy1234 <ajithreddy.pr@gmail.com>
2026-09-08 16:13:02 +0530

    Time: 28 ms (19.06%), Space: 21.8 MB (34.63%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -4,16 +4,17 @@ class Solution:
         cols=len(grid[0])
         dist=[[float("inf") for i in range(cols)] for o in range(rows)]
         dist[0][0]=grid[0][0]
-        heap=[(grid[0][0],0,0)]
-        while heap:
-            weight,x,y=heapq.heappop(heap)
-            if dist[x][y]<weight:
-                continue
-            for dx,dy in [(1,0),(0,1)]:
-                nx=x+dx
-                ny=y+dy
-                if 0<=nx<rows and 0<=ny<cols and weight+grid[nx][ny]<dist[nx][ny]:
-                    dist[nx][ny]=weight+grid[nx][ny]
-                    heapq.heappush(heap,(weight+grid[nx][ny],nx,ny))
+        for i in range(rows):
+            for j in range(cols):
+                value1=float("inf")
+                value2=float("inf")
+                if i>0:
+                    value1=dist[i-1][j]
+                if j>0:
+                    value2=dist[i][j-1]
+                if value1==value2==float("inf"):
+                    dist[i][j]=grid[i][j]
+                    continue
+                dist[i][j]=grid[i][j]+min(value1,value2)
         return dist[rows-1][cols-1]
         
