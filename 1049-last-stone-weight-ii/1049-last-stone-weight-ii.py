8cca074f983b0f5534d1d59f6b9420757f1bfb47
ajithreddy1234 <ajithreddy.pr@gmail.com>
2026-09-11 15:14:48 +0530

    Time: 23 ms (36.23%), Space: 19.3 MB (84.29%) - LeetHub

--- 1049-last-stone-weight-ii/1049-last-stone-weight-ii.py
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.py
@@ -2,15 +2,16 @@ class Solution:
     def lastStoneWeightII(self, stones: List[int]) -> int:
         n=len(stones)
         total=sum(stones)
-        dp=[0 for i in range(total+1)]
-        for cur in range(total+1):
+        target=total//2+1
+        dp=[0 for i in range(target+1)]
+        for cur in range(target+1):
             dp[cur]=abs(total-2*cur)
         for i in range(n-1,-1,-1):
-            cu=[0 for i in range(total+1)]
-            for j in range(total+1):
+            cu=[0 for i in range(target+1)]
+            for j in range(target+1):
                 no=dp[j]
                 take=float("inf")
-                if stones[i]+j<=total:
+                if stones[i]+j<=target:
                     take=dp[j+stones[i]]
                 cu[j]=min(no,take)
             dp=cu
