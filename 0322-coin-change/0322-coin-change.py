fc87332d64fc5b6d5eb391297688080d6c7f5b58
ajithreddy1234 <ajithreddy.pr@gmail.com>
2026-09-11 18:03:14 +0530

    Time: 1123 ms (9.49%), Space: 19.7 MB (38.91%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -4,13 +4,11 @@ class Solution:
         dp=[float("inf") for i in range(amount+1)]
         dp[amount]=0
         for i in range(n-1,-1,-1):
-            cu=[float("inf") for i in range(amount+1)]
             for j in range(amount,-1,-1):
                 no=dp[j]
                 take=float("inf")
                 if j+coins[i]<=amount:
-                    take=1+cu[j+coins[i]]
-                cu[j]=min(take,no)
-            dp=cu
+                    take=1+dp[j+coins[i]]
+                dp[j]=min(take,no)
         return dp[0] if dp[0]!=float("inf") else -1
 
