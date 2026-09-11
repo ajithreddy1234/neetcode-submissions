9636d1f4a0e1e23d6949249ad4abf3bbc5a8ccbe
ajithreddy1234 <ajithreddy.pr@gmail.com>
2026-09-11 15:19:32 +0530

    Time: 19 ms (55.38%), Space: 19.2 MB (92.09%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.py
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.py
@@ -1,19 +1,27 @@
-from collections import Counter
-from typing import List
-
 class Solution:
     def totalNumbers(self, digits: List[int]) -> int:
-        available = Counter(digits)
-        count = 0
-
-        for number in range(100, 1000, 2):
-            required = Counter(map(int, str(number)))
-
-            if all(required[digit] <= available[digit]
-                   for digit in required):
-                count += 1
-
+        consi=set()
+        n=len(digits)
+        s=[]
+        visited=set()
+        count=0
+        for i in range(n):
+            if digits[i]==0:
+                continue
+            for j in range(n):
+                if j==i:
+                    continue
+                for k in range(n):
+                    if k==i or k==j:
+                        continue
+                    x=digits[i]*100+digits[j]*10+digits[k]
+                    if digits[k]%2==0 and x not in visited and x>99:
+                        visited.add(x)
+                        count+=1
+                        
         return count
+
+
                     
 
 
