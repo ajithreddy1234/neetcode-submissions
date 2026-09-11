5ba0cbe8466e55bfffb9f40cf660adc1170ae0e9
ajithreddy1234 <ajithreddy.pr@gmail.com>
2026-09-11 13:53:50 +0530

    Time: 23 ms (44.84%), Space: 19.4 MB (38.46%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.py
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.py
@@ -3,30 +3,22 @@ class Solution:
         consi=set()
         n=len(digits)
         s=[]
-        count=0
         visited=set()
+        count=0
         for i in range(n):
             if digits[i]==0:
                 continue
-            s.append(str(digits[i]))
             for j in range(n):
                 if j==i:
                     continue
-                s.append(str(digits[j]))
                 for k in range(n):
                     if k==i or k==j:
                         continue
-                    s.append(str(digits[k]))
-                    x=int("".join(s))
+                    x=digits[i]*100+digits[j]*10+digits[k]
                     if digits[k]%2==0 and x not in visited and x>99:
-                        print(x)
                         visited.add(x)
                         count+=1
-                        s.pop()
-                    else:
-                        s.pop()
-                s.pop()
-            s.pop()
+                        
         return count
 
 
