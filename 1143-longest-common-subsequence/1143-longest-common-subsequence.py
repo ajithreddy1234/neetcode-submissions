1ff066a746abfc498ce880fa7f1fb8648221003c
ajithreddy1234 <ajithreddy.pr@gmail.com>
2026-09-16 22:14:12 +0530

    Time: 674 ms (22.77%), Space: 47 MB (20.74%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -3,15 +3,15 @@ class Solution:
         n = len(s1)
         m = len(s2)
 
-        memo = {}
+        dp=[[-1 for i in range((m+1))] for i in range((n+1))]
 
         def solve(i, j):
 
             if i == n or j == m:
                 return 0
 
-            if (i, j) in memo:
-                return memo[(i, j)]
+            if dp[i][j]!=-1:
+                return dp[i][j]
 
             if s1[i] == s2[j]:
                 ans = 1 + solve(i + 1, j + 1)
@@ -22,7 +22,6 @@ class Solution:
                     solve(i, j + 1)
                 )
 
-            memo[(i, j)] = ans
+            dp[i][j] = ans
             return ans
-
-        return solve(0, 0)
+        return solve(0,0)
