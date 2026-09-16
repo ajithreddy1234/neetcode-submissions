79a7a9eb356f8cd5bb0bb6e2690bc04aab7be2d5
ajithreddy1234 <ajithreddy.pr@gmail.com>
2026-09-17 02:24:46 +0530

    Time: 238 ms (85.21%), Space: 21.3 MB (51.18%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -3,17 +3,13 @@ class Solution:
         n=len(s)
         if n==1:
             return 0
-        dp=[[-1 for i in range(len(s)+1)] for i in range(len(s)+1)]
-        def solve(start,end):
-            if start>=end:
-                return 0
-            if dp[start][end]!=-1:
-                return dp[start][end]
-            if s[start]==s[end]:
-                ans=solve(start+1,end-1)
-            else:
-                ans=1+min(solve(start+1,end),solve(start,end-1))
-            dp[start][end]=ans
-            return ans
-        return solve(0,n-1)
+        dp=[[0 for i in range(len(s)+1)] for i in range(len(s)+1)]
+        for i in range(n-1,-1,-1):
+            dp[i][i]=0
+            for j in range(i+1,n):
+                if s[i]==s[j]:
+                    dp[i][j]=dp[i+1][j-1]
+                elif s[i]!=s[j]:
+                    dp[i][j]=1+min(dp[i+1][j],dp[i][j-1])
+        return dp[0][n-1] 
         
