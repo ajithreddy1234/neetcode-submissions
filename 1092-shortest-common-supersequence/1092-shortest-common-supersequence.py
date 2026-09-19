6b907576fbad7089b88a91a0a472413c45a80e19
ajithreddy1234 <ajithreddy.pr@gmail.com>
2026-09-19 18:58:15 +0530

    Time: 655 ms (6.72%), Space: 183.4 MB (5.83%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.py
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.py
@@ -1,54 +1,81 @@
 class Solution:
     def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
-        n1=len(str1)
-        n2=len(str2)
-        dp=[[float("inf") for i in range(n2+1)] for i in range(n1+1)]
-        def solve(i,j):
-            if i>=n1:
-                return n2-j
-            elif j>=n2:
-                return n1-i
-            if dp[i][j]!=float("inf"):
-                return dp[i][j]
-            if str1[i]==str2[j]:
-                cost=1+solve(i+1,j+1)
+
+        n1 = len(str1)
+        n2 = len(str2)
+
+        memo = {}
+
+        # STEP 1: Find minimum SCS length
+        def solve(i, j):
+
+            if i == n1:
+                return n2 - j
+
+            if j == n2:
+                return n1 - i
+
+            if (i, j) in memo:
+                return memo[(i, j)]
+
+            if str1[i] == str2[j]:
+
+                cost = 1 + solve(i + 1, j + 1)
+
             else:
-                cost1=1+solve(i+1,j)
-                cost2=1+solve(i,j+1)
-                if cost1<=cost2:
-                    cost=cost1
-                else:
-                    cost=cost2
-            dp[i][j]=cost
+
+                cost1 = 1 + solve(i + 1, j)
+
+                cost2 = 1 + solve(i, j + 1)
+
+                cost = min(cost1, cost2)
+
+            memo[(i, j)] = cost
+
             return cost
-        solve(0,0)
-        i=0
-        j=0
-        fin=[]
-        while i<n1 and j<n2:
-            if str1[i]==str2[j]:
-                fin.append(str1[i])
-                i+=1
-                j+=1
+
+        # Fill the memo with required states
+        solve(0, 0)
+
+        # STEP 2: Reconstruct the string
+        i = 0
+        j = 0
+
+        ans = []
+
+        while i < n1 and j < n2:
+
+            # Case 1: Characters match
+            if str1[i] == str2[j]:
+
+                ans.append(str1[i])
+
+                i += 1
+                j += 1
+
+            # Case 2: Characters differ
             else:
-                cost1=dp[i+1][j]
-                cost2=dp[i][j+1]
-                if cost1<=cost2:
-                    fin.append(str1[i])
-                    i+=1
+
+                cost1 = solve(i + 1, j)
+                cost2 = solve(i, j + 1)
+
+                if cost1 <= cost2:
+
+                    ans.append(str1[i])
+                    i += 1
+
                 else:
-                    fin.append(str2[j])
-                    j+=1
-        print(fin)
-        while i<n1:
-            fin.append(str1[i])
-            i+=1
-        while j<n2:
-            fin.append(str2[j])
-            j+=1
-        return "".join(fin)
-        
 
-                    
+                    ans.append(str2[j])
+                    j += 1
 
-        
+        # STEP 3: Append remaining characters
+        while i < n1:
+            ans.append(str1[i])
+            i += 1
+
+        while j < n2:
+            ans.append(str2[j])
+            j += 1
+
+        return "".join(ans)
