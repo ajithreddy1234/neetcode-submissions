3384c2b9fc126adc75940d80842b136bfba12c44
ajithreddy1234 <ajithreddy.pr@gmail.com>
2026-09-25 16:27:38 +0530

    Time: 291 ms (5%), Space: 34.6 MB (7.89%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,21 +1,10 @@
 class Solution:
     def maxProfit(self, nums: List[int]) -> int:
-        memo=[[-1  for i in range(2)] for i in range(len(nums)+1)]
-        def solve(i,can):
-            if i==len(nums):
-                return 0
-            if memo[i][can]!=-1:
-                return memo[i][can]
-            if can==1:
-                m=-nums[i]+solve(i+1,0)
-                n=solve(i+1,1)
-                memo[i][can]=max(m,n)
-                return memo[i][can]
-            else:
-                n=nums[i]
-                m=solve(i+1,0)
-                memo[i][can]=max(n,m)
-                return memo[i][can]
-        k=solve(0,1)
-        print(memo)
-        return k
+        n=len(nums)
+        dp=[[0  for i in range(2)] for i in range(len(nums)+1)]
+        for i in range(n):
+            dp[i][1]=nums[i]
+        for i in range(n-2,-1,-1):
+            dp[i][0]=max(dp[i+1][1]-nums[i],dp[i+1][0])
+            dp[i][1]=max(dp[i+1][1],dp[i][1])
+        return dp[0][0]
