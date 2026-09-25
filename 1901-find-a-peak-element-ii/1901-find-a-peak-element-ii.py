c93af632f0d34d524b6aae5a74e721b6e54ba5b9
ajithreddy1234 <ajithreddy.pr@gmail.com>
2026-09-25 15:21:12 +0530

    Time: 425 ms (5.02%), Space: 69.3 MB (6.45%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.py
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.py
@@ -7,16 +7,15 @@ class Solution:
         def check(i,j):
             for dx,dy in directions:
                 nx=i+dx
-                ny=i+dy
+                ny=j+dy
                 if nx<0 or nx>=rows or ny<0 or ny>=cols:
                     continue
                 if mat[nx][ny]>mat[i][j]:
                     return False
             return True
-        dq=deque([(0,0),(rows-1,cols-1),(0,cols-1),(rows-1,0)])
-        seen={((0,0),(rows-1,cols-1),(0,cols-1),(rows-1,0))}
+        dq=deque([(0,0)])
+        seen={(0,0)}
         while dq:
-            print(dq)
             i,j=dq.popleft()
             add=False
             for dx,dy in directions:
