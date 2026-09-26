a380c3a7d0c97e4a9237af61375a5865c895bdc2
ajithreddy1234 <ajithreddy.pr@gmail.com>
2026-09-26 19:09:09 +0530

    Time: 1539 ms (12.22%), Space: 120.6 MB (12.63%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -1,22 +1,23 @@
 class Solution:
     def maxProfit(self, prices: list[int]) -> int:
-        memo={}
+        n=len(prices)
+        dp=[[[-1 for i in range(3)]for i in range(2)] for i in range(n)]
         def solve(i,j,k):
             if k<=0:
                 return 0
             if i>=len(prices):
                 return 0
-            if (i,j,k) in memo:
-                return memo[(i,j,k)]
+            if dp[i][j][k]!=-1:
+                return dp[i][j][k]
             if j==0:
                 buy=-prices[i]+solve(i+1,1,k)
                 no=solve(i+1,j,k)
-                memo[(i,j,k)]=max(buy,no)
-                return memo[(i,j,k)]
+                dp[i][j][k]=max(buy,no)
+                return dp[i][j][k]
             else:
                 sell=prices[i]+solve(i+1,0,k-1)
                 no=solve(i+1,j,k)
-                memo[(i,j,k)]=max(sell,no)
-                return memo[(i,j,k)]
+                dp[i][j][k]=max(sell,no)
+                return dp[i][j][k]
         return solve(0,0,2)
         
